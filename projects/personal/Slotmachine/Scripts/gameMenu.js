76bd0a59504b076efb9e78590468ecc2f5c466fa
Robin Siegl <robin.siegl@yahoo.de>
2020-01-20 17:24:18 +0100

    Add files via upload

--- projects/personal/Slotmachine/Scripts/gameMenu.js
+++ projects/personal/Slotmachine/Scripts/gameMenu.js
@@ -2,13 +2,13 @@ let buttons = document.getElementById("gameMenu").children;     // 0 New Game - 
 
 // start new game
 buttons[0].addEventListener("click", function(){
-    console.log(document.getElementsByTagName("main")[0].children[0].classList.add("gameStarted"));
+    document.getElementsByTagName("main")[0].children[0].classList.add("gameStarted");
     newGame();
 });
 
 // load existing game if there is no save game > start a new
 buttons[1].addEventListener("click", function(){
-    console.log(document.getElementsByTagName("main")[0].children[0].classList.add("gameStarted"));
+    document.getElementsByTagName("main")[0].children[0].classList.add("gameStarted");
     loadGame();
 });
 
