cd644e3b1669fbf8fb972064d583f65cf376df56
Robin Siegl <robin.siegl@yahoo.de>
2020-01-20 17:57:10 +0100

    Add files via upload

--- projects/personal/Slotmachine/Scripts/app.js
+++ projects/personal/Slotmachine/Scripts/app.js
@@ -7,7 +7,7 @@ let interaction = document.getElementById("interaction").children;              
 let gameMessage = document.getElementById("gameMessage").children[0].children[0];                   // Output current game messages
 const assetsArray = ["Assets/Rhombus.svg","Assets/Heart.svg","Assets/BronzeCoin.svg","Assets/GreenCoin.svg","Assets/Diamond.svg"];
 const loadingArray = ["Assets/letter-C.svg","Assets/letter-A.svg","Assets/letter-S.svg","Assets/letter-I.svg","Assets/letter-N.svg","Assets/letter-O.svg"];
-const hostPath = "https://robin-siegl.github.io/projects/personal/Slotmachine/";                    // Path to the assets folder -> root path
+const hostPath = "http://localhost/Projects/Casino/Slotmachine/";                                   // Path to the assets folder -> root path
 let gameState = false;                                                                              // False = No interaction made | True = Insertcoin clicked
 let options = document.getElementById("options").children;                                          // Array with ineractions | 0 save game - 1 load game - 2 help - 3 version
 
@@ -183,7 +183,7 @@ function loadGame() {
         interaction[1].children[0].innerHTML = gameCoins + "$";
         interaction[3].children[1].innerHTML = gameDifficulty;
         updateLabels();
-        updateMessageBox("Welcome new Player!");
+        updateMessageBox("Welcome Player!");
         standardSlotdisplay();
     }
 }
@@ -263,11 +263,17 @@ function calculateCoins() {
     }
 
     if (calCoins > 0 ) {
+        console.log(playerMoney +  " + " + calCoins + " = " + (playerMoney + calCoins));
         playerMoney = playerMoney + calCoins;
+        console.log("added Coins. New Player Money: " + playerMoney));
         updateMessageBox("You won!  <span>+" + calCoins.toFixed(2) + "$</span>");
+        console.log("AFTER MESSAGEBOX: Updated player money: " + playerMoney);
         updateLabels();
+        console.log("AFTER LABELS: Updated player money: " + playerMoney);
         checkPlayerMoney();
+        console.log("AFTER CHECKPLAYERMONEY: Updated player money: " + playerMoney);
         won();
+        console.log("AFTER WON: Updated player money: " + playerMoney);
     } else {
         updateMessageBox("You lose!");
     }
