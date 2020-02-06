8c400a40692166e8448d268bf232757bf2c291da
Robin Siegl <robin.siegl@yahoo.de>
2020-02-06 19:53:27 +0100

    Add files via upload

--- projects/Countdown/app.js
+++ projects/Countdown/app.js
@@ -1,4 +1,4 @@
-let endDate = new Date(2020,1,11,0,0,0);
+let endDate = new Date(2020,1,6,0,0,0);
 let outputContainer = document.getElementById("countdownOutput");
 
 function calculateTimeRemaining(start, end) {
@@ -23,10 +23,23 @@ function outputTimeRemaining(timeArray) {
     outputContainer.children[4].children[1].innerText = timeArray[0];
 }
 
-outputTimeRemaining(defineTimeArray(calculateTimeRemaining(new Date(), endDate)));
-
 let countdown = setInterval(function() {
     let timeRemaining = calculateTimeRemaining(new Date(), endDate);
     let timeArray = defineTimeArray(timeRemaining);
     outputTimeRemaining(timeArray);
+
+    if (timeRemaining <= 0) {
+        clearInterval(countdown);
+        endAnimation();
+    }
 }, 1000);
+
+function endAnimation() {
+    while (outputContainer.children.length > 0) {
+        outputContainer.removeChild(outputContainer.children[0]);
+    }
+    let headline = document.createElement("h1");
+    headline.innerHTML = " Happy <wbr>Birthday!";
+    headline.classList.add("countdownEnd");
+    outputContainer.appendChild(headline);
+}
