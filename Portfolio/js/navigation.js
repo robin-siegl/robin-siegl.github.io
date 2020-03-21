2eb939195ad294567a0e10b2231095dd143e5906
Robin Siegl <robin.siegl@yahoo.de>
2020-03-21 17:02:58 +0100

    auto close menu on mobile

--- Portfolio/js/navigation.js
+++ Portfolio/js/navigation.js
@@ -5,18 +5,36 @@ $(document).ready(function(){
     $(".scrollToContainer a").click(function(ev){
         ev.preventDefault();
         ev.stopPropagation();
-        $('html, body').animate({scrollTop: $($(this).attr('href')).offset().top}, 500);
+
+        if ($("#mobile-nav-button").is(":visible")) {
+
+            let tempLinkObject = $(this);
+
+            $("#mobile-nav-button").html("menu").next("nav:visible").slideUp(300, function() {
+                $('html, body').animate({scrollTop: $(tempLinkObject.attr('href')).offset().top}, 500);
+            });
+
+        } else {
+
+            $('html, body').animate({scrollTop: $($(this).attr('href')).offset().top}, 500);
+            
+        }
     })
     
 
     $("#mobile-nav-button").click(function(ev){
         ev.stopPropagation();
         if ($(this).next("nav").is(":visible")) {
-            $(this).next("nav:visible").slideUp(300);
-            $(this).html("menu");
+            $(this).html("menu").next("nav:visible").slideUp(300);
         } else {
-            $(this).next("nav").slideDown(300);
-            $(this).html("close");
+            $(this).html("close").next("nav").slideDown(300);
         }
     })
 })
+
+function closeMenu(button) {
+    button.html("menu").next("nav:visible").slideUp(300);
+}
+function openMenu(button) {
+    button.html("close").next("nav").slideDown(300);
+}
