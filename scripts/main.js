b74c20fc318035ab376b4d40cada684ff6894e8a
Robin Siegl <robin.siegl@yahoo.de>
2021-07-14 14:22:01 +0200

    Added scroll to more info arrow

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,19 @@
 document.addEventListener("scroll", event => {
+    // Show or hide prfile pic in navbar
     if (document.documentElement.clientHeight > document.documentElement.scrollTop) {
         document.getElementById("profile-pic").classList.remove("show");
         document.getElementById("profile-pic").classList.add("hide");
     } else {
         document.getElementById("profile-pic").classList.remove("hide");
         document.getElementById("profile-pic").classList.add("show");
+    }
+
+    // Show or hide arrow to scroll to info
+    if (document.documentElement.clientHeight / 1.5 < document.documentElement.scrollTop) {
+        document.getElementById("scroll-to-info").classList.remove("show");
+        document.getElementById("scroll-to-info").classList.add("hide");
+    } else {
+        document.getElementById("scroll-to-info").classList.remove("hide");
+        document.getElementById("scroll-to-info").classList.add("show");
     }
 })
